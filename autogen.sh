6720c072fab8ffa62dac58958151ba4a481caf98
Byvirven <dossantos@ebu.ch>
2013-06-11 22:31:13 +0200

    First version of MXFTools installer

---
+++ autogen.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# check for libtoolize on Linux and glibtoolize on Mac
+if command -v libtoolize >/dev/null 2>&1; then
+  lt=libtoolize
+elif command -v glibtoolize >/dev/null 2>&1; then
+  lt=glibtoolize
+else
+  echo "ERROR: libtoolize or glibtoolize not found"
+  exit 1
+fi
+
+set -x
+aclocal
+$lt
+autoheader -f
+automake --foreign --add-missing -c
+autoconf
