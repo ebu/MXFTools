06fe678cf0d2bcdedb08fd45761e453ff8f0c9f2
Byvirven <dossantos@ebu.ch>
2013-08-21 18:20:26 +0200

    Change the path of gdk header for windows

--- playerEngine.hpp
+++ playerEngine.hpp
@@ -20,7 +20,7 @@
 #if defined (GDK_WINDOWING_X11)
 #include <gdk/gdkx.h>
 #elif defined (GDK_WINDOWING_WIN32)
-#include <gdk/gdkwin32.h>
+#include <gdk/win32/gdkwin32.h>
 #elif defined (GDK_WINDOWING_QUARTZ)
 #include <gdk/gdkquartz.h>
 #endif
